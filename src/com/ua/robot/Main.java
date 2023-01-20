309e9670f2eea381884b7f4ee0709a649495b7a0
Nataliia Koval <ruba.fish1500@gmail.com>
2023-01-20 10:19:20 +0200

    lesson_4

---
+++ src/com/ua/robot/Main.java
@@ -0,0 +1,18 @@
+package com.ua.robot;
+
+public class Main {
+    public static void main(String[] args) {
+        int x = 21;
+        int y = 15;
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x / y);
+        System.out.println(x * y);
+        System.out.println("_____________________________");
+        String str = "imagination";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+        System.out.println(str.charAt(0));
+        System.out.println(str.charAt(str.length() - 1));
+    }
+}
