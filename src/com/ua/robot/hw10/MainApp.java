50811b210a94a7eaa09504794c1caac674a6cb67
Nataliia Koval <ruba.fish1500@gmail.com>
2023-02-10 16:57:54 +0200

    homework for lesson 10..

--- src/com/ua/robot/hw10/MainApp.java
+++ src/com/ua/robot/hw10/MainApp.java
@@ -1,22 +1,20 @@
 package com.ua.robot.hw10;
 
-import java.util.Arrays;
-
 public class MainApp {
     public static void main(String[] args) {
         Student[] studentArray = {new Student("Henry", "Lewis", 19, 1001, "Computer Science"),
                 new Student("Ben", "Miller", 20, 1002, "Computer Science"),
                 new Student("Arabella", "Clark", 19, 1003, "Computer Science")};
-        for (Student elem : studentArray) {
-            System.out.print(elem + " ");
+        for (Student obj : studentArray) {
+            System.out.print(obj + " ");
         }
         System.out.println();
 
         Teacher[] teacherArray = {new Teacher("Andrew", "Lewis", 39, "Mathematics"),
                 new Teacher("Harry", "Rainbow", 30, "Physical culture"),
                 new Teacher("Natalie", "Crawl", 29, "English")};
-        for (Teacher elem : teacherArray) {
-            System.out.print(elem + " ");
+        for (Teacher obj : teacherArray) {
+            System.out.print(obj + " ");
         }
         System.out.println("\n");
 
