d8c238a8b2356a157c2e7148586d4bcc8cba08d3
Nataliia Koval <ruba.fish1500@gmail.com>
2023-02-14 06:55:16 +0200

    override hashcode

--- src/com/ua/robot/hw11/AutoTruck.java
+++ src/com/ua/robot/hw11/AutoTruck.java
@@ -1,5 +1,7 @@
 package com.ua.robot.hw11;
 
+import java.util.Objects;
+
 public class AutoTruck extends Car{
 
     private final int maxCargoWeight;
@@ -23,5 +25,19 @@ public class AutoTruck extends Car{
                 ", price = " + price +
                 "$, " + "maxCargoWeight = " + maxCargoWeight +
                 " kg }";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        AutoTruck autoTruck = (AutoTruck) o;
+        return maxCargoWeight == autoTruck.maxCargoWeight;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), maxCargoWeight);
     }
 }
