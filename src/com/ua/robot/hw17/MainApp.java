7478f0a058483292a19fff2ff80696395491f88c
Nataliia Koval <ruba.fish1500@gmail.com>
2023-03-07 10:44:15 +0200

    homework for lesson_17

---
+++ src/com/ua/robot/hw17/MainApp.java
@@ -0,0 +1,13 @@
+package com.ua.robot.hw17;
+
+public class MainApp {
+    public static void main(String[] args) {
+        Weekdays.getWeekdayByNumber(1);
+        Weekdays.getWeekdayByNumber(2);
+        Weekdays.getWeekdayByNumber(3);
+        Weekdays.getWeekdayByNumber(4);
+        Weekdays.getWeekdayByNumber(5);
+        Weekdays.getWeekdayByNumber(6);
+        Weekdays.getWeekdayByNumber(7);
+    }
+}
---
+++ src/com/ua/robot/hw17/Weekdays.java
@@ -0,0 +1,31 @@
+package com.ua.robot.hw17;
+
+public enum Weekdays {
+    MONDAY(1), TUESDAY(2), WEDNESDAY(3),
+    THURSDAY(4), FRIDAY(5), SATURDAY(6), SUNDAY(7);
+
+    private final int weekdayNumber;
+
+    Weekdays(int weekdayNumber) {
+        this.weekdayNumber = weekdayNumber;
+    }
+
+    static void getWeekdayByNumber(int number) {
+        switch (number) {
+            case 1 -> System.out.println(MONDAY);
+            case 2 -> System.out.println(TUESDAY);
+            case 3 -> System.out.println(WEDNESDAY);
+            case 4 -> System.out.println(THURSDAY);
+            case 5 -> System.out.println(FRIDAY);
+            case 6 -> System.out.println(SATURDAY);
+            case 7 -> System.out.println(SUNDAY);
+            default -> System.out.println("Illegal number. You can enter only integer number from 1 to 7.");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.name() + ", weekday number: " + weekdayNumber;
+    }
+
+}
