4e9d3943b9fbf6eb8964e9a9456495ce58eec7b4
Nataliia <ruba.fish1500@gmail.com>
2023-04-27 07:05:56 +0300

    hw# 32 edited

--- src/com/ua/robot/hw32/AnotherSimpleThread.java
+++ src/com/ua/robot/hw32/AnotherSimpleThread.java
@@ -4,6 +4,11 @@ public class AnotherSimpleThread implements Runnable {
     @Override
     public void run() {
         for (int i = 1; i < 11; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println("AnotherSimpleThread" + " " + i);
         }
     }
--- src/com/ua/robot/hw32/SimpleThread.java
+++ src/com/ua/robot/hw32/SimpleThread.java
@@ -4,6 +4,11 @@ public class SimpleThread implements Runnable {
     @Override
     public void run() {
         for (int i = 1; i < 11; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
             System.out.println("SimpleThread" + " " + i);
         }
     }
